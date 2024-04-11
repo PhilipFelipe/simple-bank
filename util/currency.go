8e5eefe160e0036db33ca53372b82b390f1ebb4d
felipe lencione <wakezinnsaw13@gmail.com>
2024-04-11 09:53:17 -0300

    FEAT: implemented transfer API

---
+++ util/currency.go
@@ -0,0 +1,17 @@
+package util
+
+// Constants for all supported currencies
+const (
+	USD = "USD"
+	EUR = "EUR"
+	CAD = "CAD"
+	BRL = "BRL"
+)
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, CAD, BRL:
+		return true
+	}
+	return false
+}
